5591a0ecbc5a994c800622ed413749e15e1df89d
MR <cooper.a.m@hotmail.com>
2019-08-06 05:43:22 +0100

    adas

--- Extensions/Chrome/src/popup/Update.tsx
+++ Extensions/Chrome/src/popup/Update.tsx
@@ -1,23 +1,33 @@
 import * as React from 'react';
-
 const API_ADDRESS = 'https://api.github.com/';
 
-const UpdateUserTeamForm: React.StatelessComponent<{}> = ({ }) => {
+export const Update: React.FunctionComponent<{}> = ({ }) => {
 
     const [lastUpdated, setLastUpdated] = React.useState<number>(undefined);
     const [lastVersionCheck, setLastVersionCheck] = React.useState<number>(undefined);
-    const [version, setVersion] = React.useState<string>();
+    const [version, setVersion] = React.useState<string>(undefined);
+
+    React.useEffect(() => {
+        GetLatestRelease();
+    })
 
     function GetLatestRelease() {
+        setLastVersionCheck(Date.now);
         fetch(API_ADDRESS + `repos/acoop133/githubdarktheme/releases`)
-            .then(res => res.json())
-            .then(result => {
-                setVersion(result.tag_name);
-                setLastVersionCheck(Date.now);
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setVersion(data.tag_name);
             })
+            .catch(function (error) {
+                // handle error
+                console.error(error);
+            });
+
     }
 
     return <>
+        {version === undefined && GetLatestRelease()}
         <div>Current version: {version}</div>
 
     </>
