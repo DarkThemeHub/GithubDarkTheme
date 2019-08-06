a2c12bdf779c038a3cc857164ede9f284729f785
Aaron <acoop133@hotmail.com>
2019-08-06 17:15:11 +0100

    Storage persistence and styling

--- Extensions/Chrome/src/popup/Popup.tsx
+++ Extensions/Chrome/src/popup/Popup.tsx
@@ -7,22 +7,22 @@ interface AppProps { }
 interface AppState { }
 
 export default class Popup extends React.Component<AppProps, AppState> {
-    constructor(props: AppProps, state: AppState) {
-        super(props, state);
-    }
+  constructor(props: AppProps, state: AppState) {
+    super(props, state);
+  }
 
-    componentDidMount() {
-        // Example of how to send a message to eventPage.ts.
-        chrome.runtime.sendMessage({ popupMounted: true });
-    }
+  componentDidMount() {
+    // Example of how to send a message to eventPage.ts.
+    chrome.runtime.sendMessage({ popupMounted: true });
+  }
 
-    render() {
-        return (
+  render() {
+    return (
+      <div className="popupContainer">
+        <h2>Github Darktheme</h2>
 
-            <div className="popupContainer">
-                <Update></Update>
-                <div>Hello, world!</div>
-            </div>
-        )
-    }
+        <Update />
+      </div>
+    );
+  }
 }
