d432a0882c0bfb89c5062f529e86fbf2ef705b85
MR <cooper.a.m@hotmail.com>
2019-09-06 20:51:15 +0100

    Remove theme event

--- Extensions/Chrome/dist/public/injector.js
+++ Extensions/Chrome/dist/public/injector.js
@@ -18,39 +18,14 @@ function inject() {
 
         (document.body || document.documentElement).appendChild(script);
     });
-
-    //var data = () => { await getStyle(version) };
-
-
 }
 
 document.addEventListener('injectTheme', function (e) {
     inject();
 });
 
-document.addEventListener('injectCSS', function (e) {
-    var data = e.detail;
-    console.log('received', data);
 
-    chrome.storage.local.get('storageFile', (result) => {
-        const storageFile = result.storageFile;
-        console.log(storageFile);
-        data = fetch(`https://raw.githubusercontent.com/acoop133/GithubDarkTheme/${storageFile.version}/Theme.css`).then(response => response.text()).then(css => css)
-
-    });
-
-
-    var script = document.createElement('style');
-    script.type = 'text/css'
-    script.id = 'githubdarktheme'
-    script.textContent = data;
-
-    (document.head || document.documentElement).appendChild(script);
-
-});
-
-
-document.addEventListener('removeCSS', function (e) {
+document.addEventListener('removeTheme', function (e) {
     var script = document.getElementById("githubdarktheme")
     script.remove();
 })
