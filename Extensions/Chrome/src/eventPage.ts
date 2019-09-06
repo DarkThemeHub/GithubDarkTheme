368e9864666672d762a7dfa60d9f7fa4ccfb109a
MR <cooper.a.m@hotmail.com>
2019-09-06 19:33:02 +0100

    inject on load

--- Extensions/Chrome/src/eventPage.ts
+++ Extensions/Chrome/src/eventPage.ts
@@ -1,4 +1,4 @@
-// Listen to messages sent from other parts of the extension.
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // onMessage must return "true" if response is async.
     let isResponseAsync = false;
