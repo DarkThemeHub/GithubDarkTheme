c24e5d9b65cbfb91af2ed55464cd6ff18242a1ee
MR <cooper.a.m@hotmail.com>
2019-11-14 09:25:19 +0000

    Stylus File Generator

---
+++ Scripts/GenerateStylusFile.py
@@ -0,0 +1,18 @@
+import os
+baseDir = os.path.dirname(os.path.dirname(__file__))
+
+header = open("header.txt", "r").read()
+preStyle = open("prestyle.txt", "r").read()
+themeCss = open(baseDir + "\\theme.css", "r",
+                encoding="ASCII", errors="ignore").read()
+endStyle = "}"
+
+if os.path.exists("githyb.user.styl"):
+    os.remove("github.user.style")
+
+f = open(baseDir + "\Generated\github.user.styl", "w+")
+outString = header + "\n" + preStyle + "\n" + \
+    themeCss + "\n" + endStyle
+f.writelines(outString)
+
+f.close
