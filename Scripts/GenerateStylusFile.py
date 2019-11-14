d8f99875ae935d6c618d9cb5ca6fee2b54eb8816
MR <cooper.a.m@hotmail.com>
2019-11-14 10:20:12 +0000

    try something

--- Scripts/GenerateStylusFile.py
+++ Scripts/GenerateStylusFile.py
@@ -1,15 +1,25 @@
 import os
-baseDir = os.path.dirname(os.path.realpath(__file__))
-header = open(baseDir + "/header.txt", "r").read()
-preStyle = open(baseDir + "/prestyle.txt", "r").read()
-themeCss = open(os.path.dirname(baseDir) + "/theme.css", "r",
+baseDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+if baseDir.startswith("/home/runner/work/GithubDarkTheme"):
+    baseDir = ""
+else:
+    baseDir = baseDir + "/"
+
+header = open(baseDir + "Scripts/Header.txt", "r").read()
+preStyle = open(baseDir + "Scripts/PreStyle.txt", "r").read()
+themeCss = open(baseDir + "Theme.css", "r",
                 encoding="ASCII", errors="ignore").read()
+
 endStyle = "}"
 
-if os.path.exists("githyb.user.styl"):
-    os.remove("github.user.style")
+if os.path.exists(baseDir):
+    if os.path.exists(baseDir + "githyb.user.styl"):
+        os.remove(baseDir + "github.user.style")
+else:
+    os.makedirs(baseDir + "Generated")
 
-f = open(os.path.dirname(baseDir) + "\Generated\github.user.styl", "w+")
+f = open(baseDir + "Generated/github.user.styl", "w+")
 outString = header + "\n" + preStyle + "\n" + \
     themeCss + "\n" + endStyle
 f.writelines(outString)
