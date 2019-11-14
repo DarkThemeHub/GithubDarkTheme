47badf49b9a11471acb60d7c6bdc99c056336ce9
MR <cooper.a.m@hotmail.com>
2019-11-14 10:15:58 +0000

    Fix script paths

--- Scripts/GenerateStylusFile.py
+++ Scripts/GenerateStylusFile.py
@@ -1,16 +1,15 @@
 import os
-baseDir = os.path.dirname(os.path.dirname(__file__))
-
-header = open(baseDir + "\Scripts\header.txt", "r").read()
-preStyle = open(baseDir + "\Scripts\prestyle.txt", "r").read()
-themeCss = open(baseDir + "\\theme.css", "r",
+baseDir = os.path.dirname(os.path.realpath(__file__))
+header = open(baseDir + "/header.txt", "r").read()
+preStyle = open(baseDir + "/prestyle.txt", "r").read()
+themeCss = open(os.path.dirname(baseDir) + "/theme.css", "r",
                 encoding="ASCII", errors="ignore").read()
 endStyle = "}"
 
 if os.path.exists("githyb.user.styl"):
     os.remove("github.user.style")
 
-f = open(baseDir + "\Generated\github.user.styl", "w+")
+f = open(os.path.dirname(baseDir) + "\Generated\github.user.styl", "w+")
 outString = header + "\n" + preStyle + "\n" + \
     themeCss + "\n" + endStyle
 f.writelines(outString)
