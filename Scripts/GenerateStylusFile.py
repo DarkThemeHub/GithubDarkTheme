c8871b683fbedea129522d666117278620394589
MR <cooper.a.m@hotmail.com>
2020-05-17 20:10:20 +0100

    Introduce scss

--- Scripts/GenerateStylusFile.py
+++ Scripts/GenerateStylusFile.py
@@ -3,7 +3,7 @@ import re
 import shutil
 baseDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # normalize baseDir between github action or dev environment
-if baseDir.startswith("/home/runner/work/GithubDarkTheme"):
+if baseDir.startswith("/home/runner/work"):
     baseDir = ""
 else:
     baseDir = baseDir + "/"
@@ -15,12 +15,12 @@ header = headerFile.read()
 urlRegex = open(baseDir + "UrlRegex.txt", "r").read().rstrip()
 preStyle = "@-moz-document regexp(\"<URL_REGEX>\") {"
 
-themeCss = open(baseDir + "Theme.css", "r",
+themeCss = open(baseDir + "ScssOutput/Theme.css", "r",
                 encoding="ASCII", errors="ignore").read()
 endStyle = "}"
 
-# update version in header 
-newHeader = re.sub("<Version>", newVersion , header)
+# update version in header
+newHeader = re.sub("<Version>", newVersion, header)
 newHeader = re.sub("<ThemeName>", themeName, newHeader)
 newPreStyle = re.sub("<URL_REGEX>", urlRegex, preStyle)
 # empty generated Folder
