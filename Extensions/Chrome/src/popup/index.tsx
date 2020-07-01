5f1cb1b3e4139000aee5c2a8377fd293bea634e9
MR <cooper.a.m@hotmail.com>
2020-07-01 12:01:19 +0100

    move extension into its own repo

--- Extensions/Chrome/src/popup/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Popup from './Popup';
-
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
-    ReactDOM.render(<Popup />, document.getElementById('popup'));
-});
