3c1df65ab79024b23fbe6871d65666879e4873a4
Snazzie <cooper.a.m@hotmail.com>
2020-11-23 01:20:48 +0000

    Generate userscript

--- Scripts/GenerateStylusFile.py
+++ Scripts/GenerateStylusFile.py
@@ -8,27 +8,41 @@ if baseDir.startswith("/home/runner/work"):
 else:
     baseDir = baseDir + "/"
 
-themeName = open(baseDir + "Scripts/ThemeName.txt", "r").read().rstrip()
-newVersion = open(baseDir + "Scripts/Version.txt", "r").read().rstrip()
-headerFile = open(baseDir + "Scripts/Header.txt", "r")
-header = headerFile.read()
-urlRegex = open(baseDir + "UrlRegex.txt", "r").read().rstrip()
-preStyle = "@-moz-document regexp(\"<URL_REGEX>\") {"
-
-themeCss = open(baseDir + "ScssOutput/Theme.css", "r",
-                encoding="ASCII", errors="ignore").read()
-endStyle = "}"
-
-# update version in header
-newHeader = re.sub("<Version>", newVersion, header)
-newHeader = re.sub("<ThemeName>", themeName, newHeader)
-newPreStyle = re.sub("<URL_REGEX>", urlRegex, preStyle)
 # empty generated Folder
 shutil.rmtree(baseDir + "Generated", ignore_errors=True)
 os.makedirs(baseDir + "Generated", exist_ok=True)
 
+themeName = open(baseDir + "Scripts/ThemeName.txt", "r").read().rstrip()
+newVersion = open(baseDir + "Scripts/Version.txt", "r").read().rstrip()
+urlRegex = open(baseDir + "UrlRegex.txt", "r").read().rstrip()
+themeCss = open(baseDir + "ScssOutput/Theme.css", "r",
+                encoding="ASCII", errors="ignore").read()
 
+stylusTemplate = open(baseDir + "Scripts/StylusTemplate.txt", "r")
+
+stylusFile = stylusTemplate.read()
+stylusFile = re.sub("<Version>", newVersion, stylusFile)
+stylusFile = re.sub("<ThemeName>", themeName, stylusFile)
+stylusFile = re.sub("<Url_Regex>", urlRegex, stylusFile)
+stylusFile = re.sub("<Style>", themeCss, stylusFile)
 f = open(baseDir + "Generated/github.user.styl", "w")
-outString = newHeader + "\n" + newPreStyle + "\n" + themeCss + "\n" + endStyle
-f.writelines(outString)
+
+f.writelines(stylusFile)
+f.close
+
+userScriptTemplate = open(baseDir + "Scripts/UserScriptTemplate.txt", "r")
+
+userScriptFile = userScriptTemplate.read()
+userScriptStyle = ""
+with open(baseDir + "ScssOutput/Theme.css", "r") as f:
+    for line in f:
+        userScriptStyle += "\t\t\"" + re.sub("\"", "\\\"",line.rstrip()) + "\"," + "\n"
+
+userScriptFile = re.sub("<Version>", newVersion, userScriptFile)
+userScriptFile = re.sub("<ThemeName>", themeName, userScriptFile)
+userScriptFile = re.sub("<Url_Regex>", urlRegex, userScriptFile)
+userScriptFile = re.sub("<Style>", userScriptStyle, userScriptFile)
+f = open(baseDir + "Generated/github_userscript.js", "w")
+
+f.writelines(userScriptFile)
 f.close
